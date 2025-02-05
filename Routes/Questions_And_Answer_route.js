0df558495c723c69c0196447e6c9b9f4e2a86295
drghalwash <drkhaledghalwash@gmail.com>
2025-02-05 23:16:25 +0200

    Update Questions_And_Answer_route.js

--- Routes/Questions_And_Answer_route.js
+++ Routes/Questions_And_Answer_route.js
@@ -1,5 +1,20 @@
 import { Router } from 'express';
 import { index } from '../Controller/Questions_And_Answer.js';
 const router = new Router();
-router.get('/', index);
+
+// Replace the simple route with the enhanced version
+router.get('/', async (req, res, next) => {
+    try {
+        // Get data from your controller
+        const data = await index();
+        
+        // Render the template with controller data
+        res.render('questionnaire_and_answer', {
+            ...data
+        });
+    } catch (error) {
+        next(error);
+    }
+});
+
 export default router;
