245b22f5004aaf0a61c51c98187de557f892f88d
drghalwash <drkhaledghalwash@gmail.com>
2025-02-12 00:55:52 +0200

    Update Questions_And_Answer.js

--- Controller/Questions_And_Answer.js
+++ Controller/Questions_And_Answer.js
@@ -3,7 +3,50 @@
 import Photo_Gallaries from "../DB Models/Photo_Gallary.js";
 import { readdir, readFile } from 'fs/promises';
 import { join } from 'path';
-import { categoriesConfig } from '../upload/js/categoryConfig.js';
+
+
+ const categoriesConfig = {
+    face: {
+        displayName: 'Face',
+        ids: ['rhinoplasty', 'facelift', 'eyelidlift']
+    },
+    breast: {
+        displayName: 'Breast',
+        ids: ['breastpsycho', 'lollipoptechnique', 'miniinvasivebreast', 'breastaugmentation', 'pocketlift']
+    },
+    body: {
+        displayName: 'Body',
+        ids: ['bodycontouring', 'fatgrafting', 'tummytuck', 'brazilianbuttlift', 'mommyMakeover']
+    },
+    minimallyinvasive: {
+        displayName: 'Minimally Invasive',
+        ids: ['botoxfillers', 'noninvasivecontouring']
+    },
+    other: {
+        displayName: 'Other',
+        ids: ['hairtransplant', 'skinresurfacing']
+    }
+};
+
+ const categoryDisplayNames = {
+    rhinoplasty: 'Rhinoplasty',
+    facelift: 'Facelift',
+    eyelidlift: 'Eyelid Lift',
+    breastpsycho: 'Breast Thinking all night',
+    lollipoptechnique: 'Breast Reduction Lollipop technique',
+    miniinvasivebreast: 'Thinking about mini invasive',
+    breastaugmentation: 'Breast Augmentation',
+    pocketlift: 'Pocket Lift Breast Reduction',
+    bodycontouring: 'Body Contouring',
+    fatgrafting: 'Fat Grafting',
+    tummytuck: 'Tummy Tuck Abdominoplasty',
+    brazilianbuttlift: 'Brazilian Butt Lift BBL',
+    mommyMakeover: 'Mommy Makeover',
+    botoxfillers: 'Botox Dermal Fillers',
+    noninvasivecontouring: 'Non-Invasive Body Contouring',
+    hairtransplant: 'Hair Transplant',
+    skinresurfacing: 'LASER SKIN RESURFACING'
+};
 
 export const index = async (req, res) => {
     try {
