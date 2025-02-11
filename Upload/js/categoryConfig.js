29e7a5c389cc064952cd353f9703afb66d21ae06
drghalwash <drkhaledghalwash@gmail.com>
2025-02-11 22:03:20 +0200

    Create categoryConfig.js

---
+++ Upload/js/categoryConfig.js
@@ -0,0 +1,44 @@
+// File: categoryConfig.js
+
+export const categoriesConfig = {
+    face: {
+        displayName: 'Face',
+        ids: ['rhinoplasty', 'facelift', 'eyelidlift']
+    },
+    breast: {
+        displayName: 'Breast',
+        ids: ['breastpsycho', 'lollipoptechnique', 'miniinvasivebreast', 'breastaugmentation', 'pocketlift']
+    },
+    body: {
+        displayName: 'Body',
+        ids: ['bodycontouring', 'fatgrafting', 'tummytuck', 'brazilianbuttlift', 'mommyMakeover']
+    },
+    minimallyinvasive: {
+        displayName: 'Minimally Invasive',
+        ids: ['botoxfillers', 'noninvasivecontouring']
+    },
+    other: {
+        displayName: 'Other',
+        ids: ['hairtransplant', 'skinresurfacing']
+    }
+};
+
+export const categoryMap = {
+    rhinoplasty: 'Rhinoplasty',
+    facelift: 'Facelift',
+    eyelidlift: 'Eyelid Lift',
+    breastpsycho: 'Breast Thinking all night',
+    lollipoptechnique: 'Breast Reduction Lollipop technique',
+    miniinvasivebreast: 'Thinking about mini invasive',
+    breastaugmentation: 'Breast Augmentation',
+    pocketlift: 'Pocket Lift Breast Reduction',
+    bodycontouring: 'Body Contouring',
+    fatgrafting: 'Fat Grafting',
+    tummytuck: 'Tummy Tuck Abdominoplasty',
+    brazilianbuttlift: 'Brazilian Butt Lift BBL',
+    mommyMakeover: 'Mommy Makeover',
+    botoxfillers: 'Botox Dermal Fillers',
+    noninvasivecontouring: 'Non-Invasive Body Contouring',
+    hairtransplant: 'Hair Transplant',
+    skinresurfacing: 'LASER SKIN RESURFACING'
+};
