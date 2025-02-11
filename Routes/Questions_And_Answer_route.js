f0df415a1192d6ae562460f55878fef6761748a3
drghalwash <drkhaledghalwash@gmail.com>
2025-02-11 23:08:11 +0200

    Update Questions_And_Answer_route.js

--- Routes/Questions_And_Answer_route.js
+++ Routes/Questions_And_Answer_route.js
@@ -3,14 +3,5 @@ import { index } from '../Controller/Questions_And_Answer.js';
 const router = new Router();
 router.get('/', index);
 
-router.get('/:group', async (req, res) => {
-    const group = req.params.group; // Retrieve the group from the URL
-    try {
-        res.render('Questions_And_Answer', { group: group }); // Pass the group to the template
-    } catch (error) {
-        console.error("Failed to render template:", error);
-        res.status(500).send("Internal Server Error");
-    }
-});
 
 export default router;
