939a045108c2ec6f1364063a0227c22952e9dea4
drghalwash <drkhaledghalwash@gmail.com>
2025-02-18 01:17:41 +0200

    Update qa.js

--- supabase/qa.js
+++ supabase/qa.js
@@ -1,6 +1,6 @@
 // supabase/qa.js
 
-import { createClient } from '@supabase/supabase-js'
+{--import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://drwismqxtzpptshsqphb.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRyd2lzbXF4dHpwcHRzaHNxcGhiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzk3MTExNTIsImV4cCI6MjA1NTI4NzE1Mn0.V8C0Fk9u9PS_rc3Kc-X_n-KzStr--m14fKYw9b1BJSI'
@@ -45,4 +45,4 @@ export const getZonesWithDetails = async () => {
   }
 };
 
-export default supabase;
+export default supabase;--}
