9db4b8e0f0a68ec14b5b85a5d8722dbf27e4e982
drghalwash <drkhaledghalwash@gmail.com>
2025-02-17 22:01:41 +0200

    Update server.js

--- server.js
+++ server.js
@@ -88,13 +88,6 @@ app.use('/Photo_Gallary',Photo_Gallary_route);
 app.use('/Out_of_town',Out_of_town_route);
 
 
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://drwismqxtzpptshsqphb.supabase.co'
-
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRyd2lzbXF4dHpwcHRzaHNxcGhiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzk3MTExNTIsImV4cCI6MjA1NTI4NzE1Mn0.V8C0Fk9u9PS_rc3Kc-X_n-KzStr--m14fKYw9b1BJSI'
-
-const supabase = createClient(supabaseUrl, supabaseKey)
 
 
 // Error handlers
