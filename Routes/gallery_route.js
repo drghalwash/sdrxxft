d80fa4a2075a359d1fb54fa96f054e6c04e26348
drghalwash <drkhaledghalwash@gmail.com>
2025-02-21 16:59:21 +0000

    Update gallery_route.js

--- Routes/gallery_route.js
+++ Routes/gallery_route.js
@@ -1,16 +1,13 @@
 // gallery_route.js
 import { Router } from 'express';
-import { index, publicImage, privateImage } from '../Controller/gallery.js';
+import { index, privateImage } from '../Controller/gallery.js';
 
 const router = new Router();
 
 // Route: Fetch gallery by slug (e.g., .com/galleries/Face)
 router.get('/:slug', index);
 
-// Route: Fetch public image by slug (e.g., .com/galleries/Face/rhinoplasty)
-router.get('/:gallery_slug/:image_slug', publicImage);
-
-// Route: Access private image with password (e.g., .com/galleries/Face/botox)
+// Route: Access private image with password validation (e.g., .com/galleries/Face/botox)
 router.post('/:gallery_slug/:image_slug', privateImage);
 
 export default router;
